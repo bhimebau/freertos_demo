c2bebd7bff4dcb5ec19b21eb31b9210d7f7c4cb5
Bryce Himebaugh <bhimebau@indiana.edu>
2022-04-26 08:24:48 -0400

    initial commit

---
+++ e314.mk
@@ -0,0 +1,16 @@
+# E314 Local targets
+
+download: $(BUILD_DIR)/$(TARGET).bin                                            
+	st-flash write $(BUILD_DIR)/$(TARGET).bin 0x8000000 > st-flash.log 2>&1
+
+etags:                                                                          
+	find . -type f -iname "*.[ch]" | xargs etags --append
+
+gdb:
+	cp gdbinit ./build/.gdbinit
+#	openocd -f board/st_nucleo_l4.cfg -c "stm32l4x.cpu configure -rtos FreeRTOS"
+	openocd -f board/st_nucleo_l4.cfg -c "stm32l4x.cpu configure -rtos auto"
+#	openocd -f board/st_nucleo_l4.cfg 
+reset:
+	st-flash reset
+
